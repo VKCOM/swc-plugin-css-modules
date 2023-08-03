f6cf43176ae90e87871c1bf85f97ae263e80a80d
Daniil Suvorov <severecloud@gmail.com>
2023-08-03 22:47:55 +0300

    feat: add types Autogenerated types

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,12 @@
 use serde::Deserialize;
 use serde_inline_default::serde_inline_default;
+use ts_rs::TS;
 
 // TODO: locals_convention
 
 #[serde_inline_default]
-#[derive(Debug, Deserialize, Clone)]
+#[derive(Debug, Deserialize, Clone, TS)]
+#[ts(export, export_to = "types.d.ts")]
 pub struct Config {
     #[serde_inline_default("[hash:base64]".to_string())]
     pub generate_scoped_name: String,
