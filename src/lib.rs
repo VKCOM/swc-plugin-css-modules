d9a164d63fa019c0a4876dd85e5ae70c0d41ca39
Daniil Suvorov <severecloud@gmail.com>
2024-11-05 09:30:51 +0300

    feat: update swc_core to 4.0.0 (#246)
    Update swc_core to 4.0.0
    
    https://plugins.swc.rs/versions/range/19

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,7 @@ pub mod loader_utils;
 
 pub use config::Config;
 pub use injector::Injector;
-use swc_core::ecma::{
-    ast::Program,
-    visit::{as_folder, FoldWith},
-};
+use swc_core::ecma::{ast::Program, visit::visit_mut_pass};
 use swc_core::plugin::metadata::TransformPluginMetadataContextKind;
 use swc_core::plugin::{plugin_transform, proxies::TransformPluginProgramMetadata};
 
@@ -29,7 +26,7 @@ pub fn process_transform(program: Program, metadata: TransformPluginProgramMetad
         .get_context(&TransformPluginMetadataContextKind::Cwd)
         .expect("failed to get cwd");
 
-    program.fold_with(&mut as_folder(Injector::new(
+    program.apply(visit_mut_pass(Injector::new(
         cwd.as_str(),
         filepath.as_str(),
         config,
